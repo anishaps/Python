9b3d4c1a182feab34d513d3bcd876fe5dab83bee
anishaps <a.peters-stephen@avanade.com>
2022-10-19 22:16:01 +0100

    Add numeric

--- repos/numeric.py
+++ repos/numeric.py
@@ -1,5 +1,6 @@
-i = int(2.6)
-f = (2)
+i = int(2.6) #int function rounds to the nearest integer so doesnt use decimals
+f = float(2) #float functon uses decimals 
 
-print(i)
-print(f)
+print(i) 
+print(f) 
+
