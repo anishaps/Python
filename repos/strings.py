2c4537fed8667a89c965712e6154a648b16aa586
anishaps <a.peters-stephen@avanade.com>
2022-10-19 22:01:14 +0100

    Add strings

---
+++ repos/strings.py
@@ -0,0 +1,7 @@
+sentance1 = "My name is Anisha"
+sentance2 = " I am 21 years old"
+print(sentance1 + sentance2)
+
+print("My name is Anisha","I am",21,"years old")
+
+print("My \n name \n is \n Anisha")
